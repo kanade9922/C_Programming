be796c76c515a8e0be2ea248f9266b4f705ed21a
Saurabh kanade <saurabhkanade9922@gmail.com>
2025-11-24 00:00:12 +0530

    Add files via upload

---
+++ Assignments/Assignment_26/program26_2.c
@@ -0,0 +1,59 @@
+///////////////////////////////////////////////////////////////
+//
+//   Required Header Files
+//
+///////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////
+//
+// Fuction Name :  Character Pattern Printing
+// Description :   It is used to perform no and # pattern
+// Input :         integer
+// Output :        integer
+// Author :        Saurabh Santosh Kanade
+// Date :          21/11/2025
+//
+//////////////////////////////////////////////////////////////
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = iNo; iCnt >= 1; iCnt--)
+    {
+        if(iCnt % 2 == 0)
+        {
+            printf("%d\t#\t",iCnt);
+        }
+        else
+        {
+            printf("%d\t #\t",iCnt);
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////////
+//
+// Entry Point function for the application
+//
+//////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number of elements : \n");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////
+//
+// input : 5         Output : 5  #  4  #  3  #  2  #  1  #
+//
+/////////////////////////////////////////////////////////////
---
+++ Assignments/Assignment_26/program26_4.c
@@ -0,0 +1,62 @@
+///////////////////////////////////////////////////////////////
+//
+//   Required Header Files
+//
+///////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////
+//
+// Fuction Name :  Character Pattern Printing
+// Description :   It is used to perform  #  and no and * pattern
+// Input :         integer
+// Output :        integer
+// Author :        Saurabh Santosh Kanade
+// Date :          21/11/2025
+//
+//////////////////////////////////////////////////////////////
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        if(iCnt % 2 != 0)
+        {
+            printf("#\t");
+        }
+
+        printf("%d\t",iCnt);
+
+        if(iCnt % 2 != 0)
+        {
+            printf("*\t");
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////////
+//
+// Entry Point function for the application
+//
+//////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number of elements : \n");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////
+//
+// input : 5         Output : #   1   *   #   2   *   #    3    *    #    4    *
+//
+/////////////////////////////////////////////////////////////
