869a7468fd27459c705569dfd6881c54cf4d7c4a
Saurabh kanade <saurabhkanade9922@gmail.com>
2025-11-24 00:04:00 +0530

    Add files via upload

---
+++ Assignments/Assignment_27.c/program27_3.c
@@ -0,0 +1,59 @@
+///////////////////////////////////////////////////////////////
+//
+//   Required Header Files
+//
+///////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////
+//
+// Fuction Name :  Character Pattern Printing
+// Description :   It is used to perform display rows and column using Number pattern
+// Input :         integer
+// Output :        integer
+// Author :        Saurabh Santosh Kanade
+// Date :          21/11/2025
+//
+//////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i= 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol; j >= 1; j--)
+        {
+            printf("%d\t",j);    
+        }
+        printf("\n");
+    }
+}
+
+//////////////////////////////////////////////////////////////
+//
+// Entry Point function for the application
+//
+//////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and colums :");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
+//////////////////////////////////////////////////////////////
+//
+// input : iRow : 3      iCol : 5
+//
+// Output : 5   4   3   2   1
+//          5   4   3   2   1
+//          5   4   3   2   1
+//          
+/////////////////////////////////////////////////////////////
