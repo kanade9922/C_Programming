3936c1962c42cda8f1ec5d3a7ef23404d24bca2e
Saurabh kanade <saurabhkanade9922@gmail.com>
2026-04-12 16:39:07 +0530

    Add files via upload

---
+++ C_Programming_Practice/Problems on N_numbers/NumberReverse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void Display()
+{
+    int iCnt = 0;
+
+    for(iCnt = 5; iCnt >= 0; iCnt--)
+    {
+        printf("%d\t",iCnt);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
