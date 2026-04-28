d9885de31b859dfc98e7a7c1e2d9b064eb96180c
Saurabh kanade <saurabhkanade9922@gmail.com>
2026-04-28 20:45:02 +0530

    Add files via upload

---
+++ C_Programming_Practice/Problems_on_Array/Array_mallocfunction.c
@@ -0,0 +1,36 @@
+// Static memory allocation
+
+#include<stdio.h>
+
+void Display(int Arr[],int iSize)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+}
+
+int main()
+{
+    int ilength = 0, iCnt = 0;
+    int *ptr = null;
+
+    printf("Enter numbers of elements : ");
+    scanf("%d",&ilength);
+
+    ptr = (int *)malloc(ilength * sizeof(int));
+
+    printf("Enter the elements : \n");
+    for(iCnt = 0; iCnt < ilength; iCnt++)
+    {
+        scanf("%d",&ptr[iCnt]);
+    }
+
+    Display(ptr,ilength);
+
+    free(ptr);
+    
+    return 0;
+}
